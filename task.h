2862e28e890133af62473ed4c306dbec70bf2485
AlexandruOlteanu <alexandruolteanu2001@gmail.com>
2022-01-19 22:15:04 +0200

    Update task.h

--- task.h
+++ task.h
@@ -1,23 +1,22 @@
 #ifndef TASK_H_
 #define TASK_H_
 
+#include <bits/stdc++.h>
 #include <iostream>
 #include <cstdio>
 #include <fstream>
 #include <array>
 #include <utility>
 #include <string>
-#include <filesystem>
 
-namespace fs = std::filesystem;
 
 class Task {
  public:
     virtual ~Task() = default;
     virtual void solve() = 0;
     virtual void read_problem_data() = 0;
-    virtual void formulate_oracle_question() = 0;
-    virtual void decipher_oracle_answer() = 0;
+    void formulate_oracle_question() {};
+    void decipher_oracle_answer() {};
     virtual void write_answer() = 0;
 
     /*
@@ -33,7 +32,7 @@ class Task {
      * Asks the oracle for an answer to the formulated question.
      */
     void ask_oracle() {
-        std::string solver_path = fs::current_path().string() + "/sat_oracle.py";
+        std::string solver_path = "sat_oracle.py";
         std::string command = "python3 " + solver_path + " sat.cnf sat.sol";
         std::array<char, 512> buffer{};
         std::string output;
